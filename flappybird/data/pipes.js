e5c6c52e19c3253fc047d9a6df7e64c56778cce6
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-28 15:31:11 -0600

    Update pipes.js

--- flappybird/data/pipes.js
+++ flappybird/data/pipes.js
@@ -1,1 +1,16 @@
-function pipes
+range=
+function pipes(){
+  this.x=c.width;
+  this.h=0;
+  this.draw=function(){
+    ctx.fillRect(this.x,0,20,this.h);
+    ctx.fillRect(this.x,this.h-pipeSpace,20,c.height-(h+20));
+  }
+  this.move=function(){
+    this.x--;
+  }
+  this.reset=function(){
+    this.x=c.width;
+    this.h=Math.random()*((c.width-40)+20);
+  }
+}
