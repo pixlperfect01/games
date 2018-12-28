dc0a1b3d6d835d6576260a62c5a6a8ca5f8c0151
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-28 15:22:13 -0600

    Update player.js

--- flappybird/data/player.js
+++ flappybird/data/player.js
@@ -1,1 +1,17 @@
-function 
+function player(){
+  this.x=20;
+  this.y=75;
+  this.dx=this.x+10;
+  this.dy=this.y+10;
+  this.dead=false;
+  this.score=0;
+  this.vel=0;
+  this.draw=function(){
+    ctx.beginPath();
+    ctx.fillStyle="FFFF00";
+    ctx.arc(this.dx,this.dy,10,0,2*Math.PI);
+    ctx.fill();
+  }
+  this.flap=function(){
+    this.vel=-3;
+  }
