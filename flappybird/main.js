e1ae6300fedf71b8890cb9e2411b22dee9883b30
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-28 15:46:39 -0600

    Update main.js

--- flappybird/main.js
+++ flappybird/main.js
@@ -1,3 +1,21 @@
-var c,ctx;
+var c,ctx,bird,pipe;
 function setup(){
-  
+  c=document.getElementById("myCanvas");
+  ctx.c.getContext("2d");
+  bird=new player();
+  pipe=new pipe();
+  c.addEventListener("click",bird.flap);
+  pipe.reset();
+  start();
+}
+function start(){
+  bird.fall();
+  bird.deathTest();
+  pipe.move();
+  if(pipe.x<=0)
+    pipe.reset();
+  bird.draw();
+  pipe.draw();
+  if(!bird.dead)
+    window.requestAnimationFrame(start);
+}
